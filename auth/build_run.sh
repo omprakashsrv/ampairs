c07a9ca2bc08504249ac329d76cceaa9bff20750
omdigio <omprakash@digio.in>
2023-07-18 22:33:09 +0530

    init commit for project

--- auth/build_run.sh
+++ auth/build_run.sh
@@ -2,5 +2,5 @@ chmod +x ./gradlew
 ./gradlew :auth:clean
 ./gradlew :auth:build
 ./gradlew :auth:assemble
-cp ./build/libs/auth.jar auth.jar
-java -jar auth.jar
+scp -i sandbox.pem ./build/libs/auth.jar ec2-user@3.7.215.40:microservices/auth/
+ssh -i sandbox.pem ec2-user@3.7.215.40 'java -jar ~/microservices/auth/auth.jar &disown'
