7f96f144ff832ada522f45109dc6b9bd005d42e9
omdigio <omprakash@digio.in>
2023-07-22 15:34:54 +0530

    init commit for project

--- tally/src/main/kotlin/com/ampairs/tally/config/JaxbMessageConverter.kt
+++ tally/src/main/kotlin/com/ampairs/tally/config/JaxbMessageConverter.kt
@@ -1,23 +1,17 @@
 package com.ampairs.tally.config
 
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import javax.xml.transform.Source
 import javax.xml.transform.stream.StreamSource
 
 
 class JaxbMessageConverter : Jaxb2RootElementHttpMessageConverter() {
 
-
     override fun processSource(source: Source): Source {
         val streamSource = source as StreamSource
         val inputStream = streamSource.inputStream
         val replacingInputStream = ReplacingInputStream(inputStream, "&#4;", "")
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        var length: Int
-        while (-1 != replacingInputStream.read().also { length = it }) byteArrayOutputStream.write(length)
-        streamSource.inputStream = ByteArrayInputStream(byteArrayOutputStream.toByteArray())
+        streamSource.inputStream = replacingInputStream
         return super.processSource(source)
     }
 
